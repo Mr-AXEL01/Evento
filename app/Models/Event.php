75bd6668c2da7fc69a7a828d61e3c66541b43589
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-05 04:58:13 +0100

    set the relation ship has many betwenn the reservation  and event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -31,5 +32,10 @@ class Event extends Model
     public function organiser(): BelongsTo
     {
         return $this->belongsTo(Organiser::class);
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
@@ -14,4 +15,10 @@ class Reservation extends Model
         'customer_id',
         'event_id',
     ];
+
+    #----------------- set the relationShip -----------------#
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
