ec26ab52b93b98cff652c749749c5921b4c96337
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-05 04:53:32 +0100

    set the relation ship has many betwenn the category and event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
@@ -20,4 +21,10 @@ class Event extends Model
         'organiser_id',
         'category_id',
     ];
+
+    #----------------- set the relationShip -----------------#
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
