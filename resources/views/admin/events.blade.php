c9b460d9e31154ea3659e58414b6564183ea928f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-09 21:13:51 +0100

    set route to approve the events

--- resources/views/admin/events.blade.php
+++ resources/views/admin/events.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
     <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-200 min-h-screen transition-all main">
+        <div class=" p-5 mx-5 rounded-lg">
             <div class="flex justify-between">
                 <div class="uppercase text-sky-900 font-bold text-2xl">
                     Events
                 </div>
+            </div>
             <div class="flex flex-col mt-6">
                 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
@@ -20,9 +22,6 @@
                                         Name
                                     </th>
                                     <th scope="col" class="px-4 py-3.5 text-sm font-normal text-center align-middle rtl:text-right text-sky-500">
-                                        Description
-                                    </th>
-                                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-center align-middle rtl:text-right text-sky-500">
                                         Location
                                     </th>
                                     <th scope="col" class="px-4 py-3.5 text-sm font-normal text-center align-middle rtl:text-right text-sky-500">
@@ -30,9 +29,6 @@
                                     </th>
                                     <th scope="col" class="px-4 py-3.5 text-sm font-normal text-center align-middle rtl:text-right text-sky-500">
                                         Place
-                                    </th>
-                                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-center align-middle rtl:text-right text-sky-500">
-                                        Status
                                     </th>
                                     <th scope="col" class="px-4 py-3.5 text-sm font-normal text-center align-middle rtl:text-right text-sky-500">
                                         Action
@@ -47,34 +43,24 @@
                                             <img src="{{ asset('storage/image/' . $event->cover) }}" alt="User Picture" class="w-12 h-10 rounded-full mr-2">
                                             <div class="text-sm font-medium">{{ $event->title }}</div>
                                         </td>
-                                        <td class="px-4 py-4 text-sm font-medium text-center align-middle">{{ $event->description }}</td>
                                         <td class="px-4 py-4 text-sm font-medium text-center align-middle whitespace-nowrap">{{ $event->location }}</td>
                                         <td class="px-4 py-4 text-sm font-medium text-center align-middle whitespace-nowrap">{{ date('d-m-Y', strtotime($event->date)) }}</td>
                                         <td class="px-4 py-4 text-sm text-center align-middle whitespace-nowrap">{{$event->place}}</td>
-                                        <td class="px-4 py-4 text-sm font-medium text-center align-middle whitespace-nowrap">
-                                            @if($event->status == 'pending')
-                                                <div class="text-sm text-orange-500 bg-orange-400/20 w-fit px-4 py-1 rounded-2xl">Pending</div>
-                                            @elseif($event->status == 'approved')
-                                                <div class="text-sm text-blue-500 bg-green-400/20 w-fit px-4 py-1 rounded-2xl">Approved</div>
-                                            @else
-                                                <div class="text-sm text-blue-500 bg-red-400/20 w-fit px-4 py-1 rounded-2xl">Refused</div>
-                                            @endif
-                                        </td>
                                         <td class="px-4 py-4 text-sm text-center align-middle whitespace-nowrap">
                                             <div class="flex justify-center gap-2">
-                                                <form action="" method="POST">
+                                                <form action="{{ route('admin.review.event', ['event' => $event, 'status' => 'approved']) }}" method="POST">
                                                     @csrf
                                                     @method('PATCH')
                                                     <button type="submit" class="inline-flex items-center justify-center px-2 py-2 rounded-lg transition duration-200 focus:outline-none focus:ring focus:ring-opacity-50">
-                                                        <i class="fa-solid fa-trash" style="color:red">Refuse</i>
+                                                        Approve
                                                     </button>
                                                 </form>
 
-                                                <form action="" method="POST">
+                                                <form action="{{ route('admin.review.event', ['event' => $event, 'status' => 'refused']) }}" method="POST">
                                                     @csrf
                                                     @method('PATCH')
                                                     <button type="submit" class="inline-flex items-center justify-center px-2 py-2 rounded-lg transition duration-200 focus:outline-none focus:ring focus:ring-opacity-50">
-                                                        <i class="fa-solid fa-trash" style="color:red">Refuse</i>
+                                                        Refuse
                                                     </button>
                                                 </form>
                                             </div>
--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::get('admin/categories/{category}/edit', [CategoryController::class, 'edit
 Route::put('admin/categories/{category}', [CategoryController::class, 'update'])->name('admin.categories.update');
 Route::delete('admin/categories/{category}', [CategoryController::class, 'destroy'])->name('admin.categories.destroy');
 Route::get('/admin/events', [AdminController::class, 'events'])->name('admin.events');
+Route::patch('admin/events/{event}/status/{status}', [AdminController::class, 'events'])->name('admin.review.event');
+
 
 #------------------- Organiser ----------------------#
 Route::get('/organiser/dashboard', [OrganiserController::class, 'dashboard'])->name('organiser.dashboard');
