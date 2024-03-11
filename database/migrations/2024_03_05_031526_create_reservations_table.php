33024be2780c4bc4eb36dc71297ff385b18b7e9b
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-11 04:32:13 +0000

    add a pending status for the reservations and set it as the defaults

--- database/migrations/2024_03_05_031526_create_reservations_table.php
+++ database/migrations/2024_03_05_031526_create_reservations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->id();
-            $table->enum('status',['approved','refused']);
+            $table->enum('status',['approved','refused', 'pending'])->default('pending');
             $table->foreignId('customer_id')->constrained('customers')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('event_id')->constrained('events')->cascadeOnDelete()->cascadeOnUpdate();
             $table->timestamps();
--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,7 @@ Route::get('/organiser/reservations', [OrganiserController::class, 'reservations
 #------------------- Customer ----------------------#
 Route::get('/customer/dashboard', [CustomerController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('customer.dashboard');
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
