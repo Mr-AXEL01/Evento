8f89183842e89fb1f3a4665835e9e2b7f62ab8d4
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-03-11 03:46:05 +0000

    create a side bar for the user that not loged in yet 'just a viewer'

---
+++ resources/views/components/sidebar-viewer-dash.blase.php
@@ -0,0 +1,64 @@
+
+<div class="fixed left-0 top-0 w-64 h-full bg-[#f8f4f3] p-4 z-50 sidebar-menu transition-transform">
+    <a href="/" class="flex items-center pb-4 border-b border-b-gray-800">
+        <h2 class="font-bold text-2xl">EVE<span class="bg-[#831843] text-white px-2 rounded-md">NTO</span></h2>
+    </a>
+    <ul class="mt-4">
+
+        <div id="menu" class="flex flex-col space-y-2 ">
+            <a
+                href="{{ route('organiser.dashboard') }}"
+                class="text-sm font-medium text-gray-700 py-3 px-2 hover:bg-[#831843] hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out mt-4"
+            >
+                <svg
+                    class="w-6 h-6 fill-current inline-block"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
+                    ></path>
+                </svg>
+                <span class="">Dashboard</span>
+            </a>
+            <a
+                href="{{ route('organiser.events') }}"
+                class="text-sm font-medium text-gray-700 py-3 px-2 hover:bg-[#831843] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+            >
+                <svg
+                    class="w-6 h-6 fill-current inline-block"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"
+                    ></path>
+                </svg>
+                <span class="">Events</span>
+            </a>
+            <a
+                href="{{ route('organiser.reservations') }}"
+                class="text-sm font-medium text-gray-700 py-3 px-2 hover:bg-[#831843] hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+            >
+                <svg
+                    class="w-6 h-6 fill-current inline-block"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                    <path
+                        fill-rule="evenodd"
+                        d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                        clip-rule="evenodd"
+                    ></path>
+                </svg>
+                <span class="">Customers</span>
+            </a>
+        </div>
+    </ul>
+</div>
+<div class="fixed top-0 left-0 w-full h-full bg-black/50 z-40 md:hidden sidebar-overlay"></div>
+
